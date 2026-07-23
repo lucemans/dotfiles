62602d2173c4da39d5db44428db58daf9de07791
Luc <luc@lucemans.nl>
2026-07-23 22:55:20 +0200

    Update mission

--- hosts/mission/configuration.nix
+++ hosts/mission/configuration.nix
@@ -44,7 +44,7 @@
       enable = true;
       settings = {
         server = {
-          http_addr = "127.0.0.1";
+          http_addr = "10.90.0.60";
           http_port = 3000;
         };
         analytics.reporting_enabled = false;
