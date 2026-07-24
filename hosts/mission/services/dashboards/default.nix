bcf3d5b774fa78e516b2ed090bbd54d5a51820c3
Luc <luc@lucemans.nl>
2026-07-24 15:46:32 +0200

    Update mission

--- hosts/mission/services/dashboards/default.nix
+++ hosts/mission/services/dashboards/default.nix
@@ -1,10 +1,11 @@
 {pkgs}: let
+  overviewDashboard = pkgs.writeTextDir "overview.json" (builtins.readFile ./overview.json);
   homelabDashboard = pkgs.writeTextDir "homelab.json" (builtins.toJSON (import ./homelab-dashboard.nix));
 in
   pkgs.symlinkJoin {
     name = "mission-dashboards";
     paths = [
-      ./overview.json
+      overviewDashboard
       homelabDashboard
     ];
   }
