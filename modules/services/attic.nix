a871a29ef50f71c5da0917004cc21175b21f0ed9
Luc <luc@lucemans.nl>
2026-08-26 02:49:45 +0200

    update attic

--- modules/services/attic.nix
+++ modules/services/attic.nix
@@ -21,7 +21,7 @@
       environmentFile = config.sops.templates.atticd_env.path;
 
       settings = {
-        listen = "0.0.0.0:8080";
+        listen = "0.0.0.0:8082";
         jwt = {};
         chunking = {
           # The minimum NAR size to trigger chunking
