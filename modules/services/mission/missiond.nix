dbf8f6befc7c93218d6c2ab468640bc8c7c1e38a
Luc <luc@lucemans.nl>
2026-08-28 16:24:40 +0200

    update missiond

--- modules/services/mission/missiond.nix
+++ modules/services/mission/missiond.nix
@@ -16,6 +16,23 @@
       mode = "0400";
     };
 
+    sops.secrets.missiond_ics_ef = {
+      owner = "luc";
+      mode = "0400";
+    };
+    sops.secrets.missiond_ics_ef_protocol = {
+      owner = "luc";
+      mode = "0400";
+    };
+    sops.secrets.missiond_ics_v3x = {
+      owner = "luc";
+      mode = "0400";
+    };
+    sops.secrets.missiond_ics_personal = {
+      owner = "luc";
+      mode = "0400";
+    };
+
     services.missiond = {
       enable = true;
       user = "luc";
@@ -77,6 +94,34 @@
         media = {
           "doorbell.webm" = "/home/luc/doorbell.webm";
         };
+
+        calendars = {
+          ef = {
+            name = "EF";
+            url.file = config.sops.secrets.missiond_ics_ef.path;
+            window = "12h";
+            leads = ["5m" "0s"];
+          };
+          ef_protocol = {
+            name = "EF Protocol";
+            url.file = config.sops.secrets.missiond_ics_ef_protocol.path;
+            window = "12h";
+            leads = ["5m" "0s"];
+          };
+          v3x = {
+            name = "V3X";
+            url.file = config.sops.secrets.missiond_ics_v3x.path;
+            window = "12h";
+            leads = ["5m" "0s"];
+          };
+          personal = {
+            name = "Personal";
+            url.file = config.sops.secrets.missiond_ics_personal.path;
+            window = "12h";
+            leads = ["5m" "0s"];
+          };
+        };
+        calendarDefaults.poll = "5m";
 
         playlists.mission-display = {
           name = "Mission Display";
